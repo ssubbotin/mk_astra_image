8f45fb1509aa3a29b8adf1c27e96998a4f96eea9
Sergey Subbotin <ssubbotin@gmail.com>
2022-03-23 12:56:10 +0300

    remove unused code, update bootstrap path, add bootstrap script symlink

--- mk_astra_image.sh
+++ mk_astra_image.sh
@@ -1,35 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 #
-# Create a base Astra Linux Docker image.
+# Create Astra Linux Docker image
 #
-#
 
-set -ex
-
-usage() {
-	cat <<EOOPTS
-	$(basename $0) <name>
-	ACHTUNG:
-	You need to specify name of resulting tarboll.
-EOOPTS
-	exit 1
-}
-
-apt_sources=/etc/apt/sources.list
-
-# we have to do a little fancy footwork to make sure "rootfsDir" becomes the second non-option argument to debootstrap
-
-shift $((OPTIND -1))
-name=$1
-
-if [[ -z $name ]]; then
-	usage
-fi
-
-# get path to "chroot" in our current PATH
-chrootPath="$(type -P chroot)"
-
-target=$(mktemp -d --tmpdir $(basename $0).XXXXXXXX)
+result=$PWD/orel.tar
+target=$(mktemp -d --tmpdir "$(basename $0)".XXXXXXXX)
 
 mkdir -m 755 "$target"/dev
 mknod -m 600 "$target"/dev/console c 5 1
@@ -43,23 +18,23 @@ mknod -m 666 "$target"/dev/tty0 c 4 0
 mknod -m 666 "$target"/dev/urandom c 1 9
 mknod -m 666 "$target"/dev/zero c 1 5
 
-debootstrap orel "$target" http://mirror.yandex.ru/astra/current/orel/repository
+ln -sf sid /usr/share/debootstrap/scripts/orel
+
+debootstrap --no-check-gpg --variant=minbase \
+  --include=apt-transport-https,ca-certificates \
+  --components=main,contrib,non-free \
+  orel "$target" https://mirror.yandex.ru/astra/stable/orel/repository/
 
 rm -rf "$target"/usr/{{lib,share}/locale,{lib,lib64}/gconv,bin/localedef,sbin/build-locale-archive}
-#  docs
 rm -rf "$target"/usr/share/{man,doc,info,gnome/help}
-#  cracklib
 rm -rf "$target"/usr/share/cracklib
-#  i18n
 rm -rf "$target"/usr/share/i18n
-#  sln
 rm -rf "$target"/sbin/sln
-#  ldconfig
 rm -rf "$target"/etc/ld.so.cache
 rm -rf "$target"/var/cache/ldconfig/*
 
 cd "$target"
-
-tar --numeric-owner -cf /root/"$name".tar .
-
+tar --numeric-owner -cf "$result" .
 rm -rf "$target"
+
+docker import "$result" astra-linux/orel:latest
